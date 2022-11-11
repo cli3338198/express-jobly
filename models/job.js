5c417ce4c973af1d44145e815ae94918cdf1bb9a
Gordon Ball <gball261@gmail.com>
2022-11-10 20:39:25 -0500

    Started creating model/tests for jobs

--- models/job.js
+++ models/job.js
@@ -5,6 +5,33 @@ const { BadRequestError, NotFoundError } = require("../expressError");
 const { makeWhere } = require("../helpers/makeWhere");
 const { sqlForPartialUpdate } = require("../helpers/sql");
 
-class Job {}
+class Job {
+
+  // const duplicateCheck = await db.query(
+  //   `SELECT handle
+  //          FROM companies
+  //          WHERE handle = $1`,
+  //   [handle]
+  // );
+
+  static async create({ title, salary, equity, company_handle }) {
+    const result = await db.query(
+      `INSERT INTO jobs(
+          title,
+          salary,
+          equity,
+          company_handle)
+           VALUES
+             ($1, $2, $3, $4)
+           RETURNING title, salary, equity, company_handle, id`,
+      [title, salary, equity, company_handle]
+    );
+    const job = result.rows[0];
+
+    return job;
+  }
+}
+
+
 
 module.exports = Job;
--- models/job.test.js
+++ models/job.test.js
@@ -17,6 +17,48 @@ afterAll(commonAfterAll);
 
 /************************************** create */
 
+describe("create", function () {
+  const newJob = {
+    title: "Baker",
+    salary: 100000,
+    equity: 0.5,
+    company_handle: "c1",
+
+  };
+
+  test("works", async function () {
+    let job = await Job.create(newJob);
+    newJob.equity = String(newJob.equity);
+    expect(job).toEqual(newJob);
+    console.log(job);
+    const result = await db.query(
+      `SELECT title, salary, equity, company_handle
+           FROM jobs
+           WHERE title = $1`, [job.id]
+    );
+
+    expect(result).toEqual([
+      {
+        title: "Baker",
+        salary: 100000,
+        id: job.id,
+        equity: "0.5",
+        company_handle: "c1"
+      },
+    ]);
+  });
+
+  // test("bad request with dupe", async function () {
+  //   try {
+  //     await Job.create(newJob);
+  //     await Job.create(newJob);
+  //     throw new Error("fail test, you shouldn't get here");
+  //   } catch (err) {
+  //     expect(err instanceof BadRequestError).toBeTruthy();
+  //   }
+  // });
+});
+
 /************************************** findAll */
 
 /************************************** get */
